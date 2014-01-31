efd63118089d7defe224f293f8d126b28b85c070
Meghan Hade <mmhade@gmail.com>
2014-01-31 07:19:33 -0800

    Changed number of tweets called

--- ebook.rb
+++ ebook.rb
@@ -55,13 +55,13 @@ unless rand_key == 0 || params["force"]
 else
   # Fetch a thousand tweets
   begin
-    user_tweets = Twitter.user_timeline($source_account, :count => 200, :trim_user => true, :include_rts => false)
+    user_tweets = Twitter.user_timeline($source_account, :count => 2000, :trim_user => true, :include_rts => true)
     max_id = user_tweets.last.id
     source_tweets += filtered_tweets(user_tweets)
   
     # Twitter only returns up to 3200 of a user timeline, includes retweets.
     17.times do
-      user_tweets = Twitter.user_timeline($source_account, :count => 200, :trim_user => true, :max_id => max_id - 1, :include_rts => false)
+      user_tweets = Twitter.user_timeline($source_account, :count => 2000, :trim_user => true, :max_id => max_id - 1, :include_rts => true)
       puts "MAX_ID #{max_id} TWEETS: #{user_tweets.length}"
       break if user_tweets.last.nil?
       max_id = user_tweets.last.id
